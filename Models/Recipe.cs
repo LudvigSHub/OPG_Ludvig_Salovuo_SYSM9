74a2050d340a47de8eddd0dacd08717d8dd1bb07
LudvigSHub <Ludvig.salovuo@gmail.com>
2025-10-29 10:32:50 +0100

    -Påbörjat models för Recipe

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 namespace CookMaster.Models
 {
-    internal class Recipe
+    public class Recipe
     {
+        public string Title { get; set; } = string.Empty;
+        public List<string> Ingredients { get; set; } = new List<string>();
+        public string Instructions { get; set; } = string.Empty;
+        public RecipeCategory Category { get; set; } = RecipeCategory.Other;
+        public DateTime Date { get; init; } = DateTime.UtcNow;
 
+        public string OwnerUsername { get; set; } = string.Empty;
+
+    }
+
+    public Recipe(string title, List<string> ingredients, string instructions, RecipeCategory category, string ownerUsername)
+        {
+            Title = title;
+            Ingredients = ingredients;
+            Instructions = instructions;
+            Category = category;
+            OwnerUsername = ownerUsername;
+        }
     }
 }
